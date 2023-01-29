770ee1c4d311a99c8d2710f27175792eda7af300
Shubham Thakur <sbmthakur@gmail.com>
2023-01-29 12:09:28 -0500

    Use better-sqlite3 and typeorm

---
+++ code/entity/User.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column("text")
+    firstName: string
+
+    @Column("text")
+    lastName: string
+
+    @Column()
+    age: number
+
+}
--- code/server.ts
+++ code/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
 dotenv.config();
 const cors = require('cors');
 import { userRouter } from './user';
 import { linkRouter } from './link';
+import { AppDataSource } from './data-source';
+import { User } from './entity/User';
 
 const app = express()
 app.use(express.json())
@@ -24,4 +25,25 @@ app.use('/api', linkRouter)
 
 app.listen(port, () => {
     console.log(`Server started on ${port}`)
+    AppDataSource.initialize()
+        .then(async () => {
+            console.log("Data Source init!")
+            console.log("Inserting a new user into the database...")
+            const user = new User()
+            user.firstName = "Timber"
+            user.lastName = "Saw"
+            user.age = 25
+            await AppDataSource.manager.save(user)
+            console.log("Saved a new user with id: " + user.id)
+
+            console.log("Loading users from the database...")
+            const users = await AppDataSource.manager.find(User)
+            console.log("Loaded users: ", users)
+
+            console.log("Here you can setup and run express / fastify / any other framework.")
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
 })
