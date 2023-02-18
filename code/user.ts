5a92cb6d12b8b698d82a8ce8c5fff1dcca43bf98
Kumar Saurabh <kumarsaurabh737@gmail.com>
2023-02-17 20:46:04 -0500

    removed route

--- code/user.ts
+++ code/user.ts
@@ -6,18 +6,6 @@ const jsonParser = bodyParser.json();
 
 const router = Router();
 
-router.get('/info', async (request: Request, response: Response) => {
-    const prisma = (request as RequestWithPrisma).prisma
-
-    const usersWithPosts = await prisma.user.findMany({
-        include: {
-            posts: true,
-        },
-    })
-    response.send(usersWithPosts)
-    //response.send({ "done": true })
-})
-
 router.post('/login', jsonParser, (request: Request, response: Response) => {
     response.send({ "done": true })
 })
