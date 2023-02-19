d798c47c38c5bdea3ca76b8ee285436df18bdd09
Sansbailmare <bailmaresanket310@gmail.com>
2023-02-18 20:22:59 -0500

    Reigtration and Login API's added

--- code/user.ts
+++ code/user.ts
@@ -1,29 +1,83 @@
 import { Request, Response, Router } from "express";
 import bodyParser from 'body-parser';
 import { RequestWithPrisma } from "./server";
+import bcrypt from 'bcrypt';
 
 const jsonParser = bodyParser.json();
 
 const router = Router();
 
+interface User {
+  first_name: string;
+  last_name?: string;
+  email: string;
+  password: string;
+}
+
 router.get('/info', async (request: Request, response: Response) => {
-    const prisma = (request as RequestWithPrisma).prisma
+    // const prisma = (request as RequestWithPrisma).prisma
 
-    const usersWithPosts = await prisma.user.findMany({
-        include: {
-            posts: true,
-        },
-    })
-    response.send(usersWithPosts)
-    //response.send({ "done": true })
-})
-
-router.post('/login', jsonParser, (request: Request, response: Response) => {
+    // const usersWithPosts = await prisma.user.findMany({
+    //     include: {
+    //         posts: true,
+    //     },
+    // })
+    // response.send(usersWithPosts)
     response.send({ "done": true })
 })
 
-router.post('/register', jsonParser, (request: Request, response: Response) => {
-    response.send({ "done": true })
+router.post('/login', jsonParser, async (request: Request, response: Response) => {
+     const prisma = (request as RequestWithPrisma).prisma
+
+        const { email, password } = request.body;
+
+        if (!email || !password) {
+            return response.status(400).send('Missing required fields');
+        }
+
+        const user = await prisma.user.findUnique({ where: { email },
+                                            select: { email: true, password: true }, 
+                                            });
+
+
+        if (!user) {
+            return response.status(401).send('Invalid email or password');
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password as string);
+
+        if (!passwordMatch) {
+            return response.status(401).send('Invalid email or password');
+        }
+  
+        response.send({ "success": true })
+})
+
+router.post('/register', jsonParser, async (request: Request, response: Response) => {
+
+   const prisma = (request as RequestWithPrisma).prisma
+
+    const { firstName,lastName, email, password } = request.body;
+
+        if (!firstName || !email || !password) {
+            return response.status(400).send('Missing required fields');
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser: User = {
+            first_name: firstName as string,
+            last_name : lastName as string,
+            email: email as string,
+            password: hashedPassword as string,
+        };
+        try {
+        await prisma.user.create({ data: newUser });
+        response.status(201).send({ "success": true });
+    } catch (error) {
+        console.error(error);
+        response.status(500).send({ "success": false });
+    }
+
 })
 
 export const userRouter = router;
